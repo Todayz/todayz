3c3484cc690b37063eb97db117806f80fd528053
Lim Jungmuk <hypnodisc@naver.com>
2016-03-14 21:05:21 +0900

    server 실행 시 기본으로 들어갈 데이터 추가.(현재는 임시 데이터로 사용)

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into image (image_id, image_name, item_id, type) values (1, 'jmlimpng', null, 'png')
+insert into member (member_id, auth_name, birthday, description, join_date, name, password, phone_number, image_id) values (1, 'jmlimid', '1986-08-30', '안녕하세요', '1986-08-30', '임정묵', 'jmlimpass', '010-8791-1883', 1)
+
+insert into image (image_id, image_name, item_id, type) values (2, 'jmlimpng2', null, 'png')
+insert into member (member_id, auth_name, birthday, description, join_date, name, password, phone_number, image_id) values (2, 'jmlimid2', '1986-08-30', '안녕하세요', '1986-08-30', '임정묵', 'jmlimpass', '010-8791-1883', 2)
